46fb2f525b90a70b55df465d76a87ae1303c60f8
subhash magdum <subhash.magdum@outlook.com>
2025-02-12 16:01:44 +0530

    Repo Clean up for PICO 2

--- FreeRTOS/Demo/FreeRTOSBlink/build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/portalgun/D-Space/Test/FreeRTOS-Learning/FreeRTOS/Demo/FreeRTOSBlink/build/_deps/picotool/"
-};
